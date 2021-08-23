648fdf9a6907163886dd1fc49443685fe1951cc9
Jack O'Connor <oconnor663@gmail.com>
2021-08-23 17:08:17 -0400

    arc_refcell

--- arc_refcell/src/main.rs
+++ arc_refcell/src/main.rs
@@ -1,14 +1,15 @@
+use std::cell::{RefCell, RefMut};
 use std::ops::DerefMut;
-use std::sync::{Arc, RwLock, RwLockWriteGuard};
+use std::sync::Arc;
 
 fn main() {
-    let my_string = Arc::new(RwLock::new(String::new()));
+    let my_string = Arc::new(RefCell::new(String::new()));
     let mut threads = Vec::new();
     for _ in 0..10 {
         let my_string = my_string.clone();
         let thread = std::thread::spawn(move || {
-            // my_string.write().unwrap().push_str("x");
-            let mut guard: RwLockWriteGuard<String> = my_string.write().unwrap();
+            // my_string.borrow_mut().push_str("x");
+            let mut guard: RefMut<String> = my_string.borrow_mut();
             let my_string_mut: &mut String = guard.deref_mut();
             my_string_mut.push_str("x");
         });
